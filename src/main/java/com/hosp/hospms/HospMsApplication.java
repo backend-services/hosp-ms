7982927054b06a42731c5da09ee4c340d5ba1b1d
Samuel Rocha <samuelrrocha2016@gmail.com>
2019-07-16 11:15:41 -0300

    start project

---
+++ src/main/java/com/hosp/hospms/HospMsApplication.java
@@ -0,0 +1,13 @@
+package com.hosp.hospms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospMsApplication.class, args);
+	}
+
+}
