961dd82e2307f9cf0fe3572438c5cf71cda80146
Samuel Rocha <samuelrrocha2016@gmail.com>
2019-07-28 23:36:48 -0300

    Resolves: #3
    create method to list products

--- src/main/java/com/hosp/hospms/configs/DTOMapperImp.java
+++ src/main/java/com/hosp/hospms/configs/DTOMapperImp.java
@@ -3,6 +3,8 @@ package com.hosp.hospms.configs;
 import com.hosp.hospms.models.DTOs.product.EquipmentDTO;
 import com.hosp.hospms.models.DTOs.product.MedicineDTO;
 import com.hosp.hospms.models.DTOs.product.ProductDTO;
+import com.hosp.hospms.models.domains.product.Equipment;
+import com.hosp.hospms.models.domains.product.Medicine;
 import com.hosp.hospms.models.domains.product.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -22,6 +24,18 @@ public class DTOMapperImp {
 
         if(productDTO instanceof EquipmentDTO)
             return mapper.toEquipament((EquipmentDTO) productDTO);
+
+        throw new IllegalArgumentException();
+    }
+
+
+    public ProductDTO toProductDTO(Product product){
+
+        if(product instanceof Medicine)
+            return mapper.toMedicineDTO((Medicine) product);
+
+        if(product instanceof Equipment)
+            return mapper.toEquipamentDTO((Equipment) product);
 
         throw new IllegalArgumentException();
     }
